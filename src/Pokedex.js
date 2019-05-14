93bf1fe746f9ed23c9b256b063801b7bdba9c810
Greg Thomas <SilentGrig@gmail.com>
2019-05-14 19:20:28 +0100

    finalise initial version

--- src/Pokedex.js
+++ src/Pokedex.js
@@ -1,25 +1,32 @@
 import React, { Component } from 'react';
 import './Pokedex.css';
 import Pokecard from './Pokecard';
+import pokemon from './pokemon';
 
 const pokemonTeamOne = [];
 const pokemonTeamTwo = [];
 
 for (let i = 0; i < 4; i++) {
+  const poke = pokemon[i];
+  const pokemon_url = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${poke.id}.png`
+
   pokemonTeamOne.push(<Pokecard
-    name="Charmander"
-    img="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/4.png"
-    type="fire"
-    exp={62}
+    name={poke.name}
+    img={pokemon_url}
+    type={poke.type}
+    exp={poke.base_experience}
   />);
 };
 
-for (let i = 0; i < 4; i++) {
+for (let i = 4; i < 8; i++) {
+  const poke = pokemon[i];
+  const pokemon_url = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${poke.id}.png`
+
   pokemonTeamTwo.push(<Pokecard
-    name="Charmander"
-    img="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/4.png"
-    type="fire"
-    exp={62}
+    name={poke.name}
+    img={pokemon_url}
+    type={poke.type}
+    exp={poke.base_experience}
   />);
 };
 
@@ -27,7 +34,7 @@ class Pokedex extends Component {
   render() {
     return (
       <div className="Pokedex">
-        <h1>Pokedex</h1>
+        <h1 className="Pokedex-title">Pokedex</h1>
         <div className="Pokedex-row">
           {pokemonTeamOne}
         </div>
