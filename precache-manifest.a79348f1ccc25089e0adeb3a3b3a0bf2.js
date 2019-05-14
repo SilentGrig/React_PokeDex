5486ecfefd5162cecb05a9d50ec653f4f635d067
Greg Thomas <SilentGrig@gmail.com>
2019-05-14 19:40:41 +0100

    Updates

---
+++ precache-manifest.a79348f1ccc25089e0adeb3a3b3a0bf2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3457e82c310e3addb342",
+    "url": "/React_PokeDex/static/js/runtime~main.72f9d5da.js"
+  },
+  {
+    "revision": "8386fcd20317bff9f75b",
+    "url": "/React_PokeDex/static/js/main.28ff7ee4.chunk.js"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/React_PokeDex/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "8386fcd20317bff9f75b",
+    "url": "/React_PokeDex/static/css/main.1e11cbac.chunk.css"
+  },
+  {
+    "revision": "f4067507a950fbd2155474a870bdf222",
+    "url": "/React_PokeDex/index.html"
+  }
+];
