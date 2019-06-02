a0cec4ed69afdcfb79df22dce8de7bbb3954c818
Greg Thomas <SilentGrig@gmail.com>
2019-06-02 13:39:26 +0100

    Updates

---
+++ precache-manifest.ccb5dd6172259f5eb9aacd3c0b2e48c8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3457e82c310e3addb342",
+    "url": "/React_PokeDex/static/js/runtime~main.72f9d5da.js"
+  },
+  {
+    "revision": "fb725a77df3a509630b3",
+    "url": "/React_PokeDex/static/js/main.5237f262.chunk.js"
+  },
+  {
+    "revision": "b5557015013d0ebce64f",
+    "url": "/React_PokeDex/static/js/2.e7cec38b.chunk.js"
+  },
+  {
+    "revision": "fb725a77df3a509630b3",
+    "url": "/React_PokeDex/static/css/main.2870a57d.chunk.css"
+  },
+  {
+    "revision": "613b907b95d1afcc3372fa4939a55b3a",
+    "url": "/React_PokeDex/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/React_PokeDex/precache-manifest.07729b8a6284fec88324530c1ada2fa7.js"
+  "/React_PokeDex/precache-manifest.ccb5dd6172259f5eb9aacd3c0b2e48c8.js"
 );
 
 workbox.clientsClaim();
